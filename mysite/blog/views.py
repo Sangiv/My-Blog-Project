3077fed3184a8a6d6ae9c67b51cea14b645e016d
SangivGiovanni <sangivgiovanni@aol.com>
2020-06-24 22:17:13 +0100

    Set up urls, views and templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView
+                                    )
 
 # Create your views here.
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    
