2a61be4b2e2520d847ecce194a00aec3ca7d4a42
SangivGiovanni <sangivgiovanni@aol.com>
2020-06-26 22:12:13 +0100

    deploying blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -30,11 +30,11 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now())
-    approved_comment = models.BooleanField(default=False)
-    
-    def approve(self):
-        self.approved_comment = True
-        self.save()
+    # approved_comment = models.BooleanField(default=False)
+    #
+    # def approve(self):
+    #     self.approved_comment = True
+    #     self.save()
 
     def get_absolute_url(self):
         return reverse('post_list')
